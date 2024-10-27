5e3a2f32039d0766fb9592ac5795560462f886bd
psaavala <c3sahe00@students.oamk.fi>
2024-10-27 17:31:14 +0200

    updated productform

--- src/ProductForm.jsx
+++ src/ProductForm.jsx
@@ -1,25 +1,21 @@
 import { useState } from 'react';
+import OrderInfo from './OrderInfo.jsx';
 
 function ProductForm() {
-  // Arrays for product names and prices
   const productNames = ["AMD Ryzen 5 3600", "Intel Core i5-10600K", "AMD Ryzen 7 3700X"];
   const productPrices = [245, 280, 320];
 
-  // States for selected product and quantity
-  const [selectedProductIndex, setSelectedProductIndex] = useState(0);  // Default product
-  const [quantity, setQuantity] = useState(1);  // Default quantity is 1
+  const [selectedProductIndex, setSelectedProductIndex] = useState(0);
+  const [quantity, setQuantity] = useState(1);  
 
-  // dropdown product selection
   const handleProductChange = (e) => {
     setSelectedProductIndex(e.target.value);
   };
 
-  // Increase quantity
   const increaseQuantity = () => {
     setQuantity(quantity + 1);
   };
 
-  // Check for minus quantity
   const decreaseQuantity = () => {
     if (quantity > 0) {
       setQuantity(quantity - 1);
@@ -30,7 +26,6 @@ function ProductForm() {
     <div>
       <h2>Select product</h2>
       <label>Product:</label>
-      {/* Dropdown for selecting a product */}
       <select value={selectedProductIndex} onChange={handleProductChange}>
         {productNames.map((product, index) => (
           <option key={index} value={index}>
@@ -41,10 +36,15 @@ function ProductForm() {
 
       <br />
       <label>Quantity:</label>
-      {/* Buttons for adjusting the quantity */}
       <button onClick={decreaseQuantity}>-</button>
       <span>{quantity}</span>
       <button onClick={increaseQuantity}>+</button>
+
+      <OrderInfo
+        productName={productNames[selectedProductIndex]}
+        productPrice={productPrices[selectedProductIndex]}
+        quantity={quantity}
+      />
     </div>
   );
 }
